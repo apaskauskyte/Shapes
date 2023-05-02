55ed64829f09bfb28c2ea57691574e169cfaf7cc
apaskauskyte <a.paskauskyte@gmail.com>
2023-05-02 13:40:25 +0300

    Add fix for Circle class

--- app/src/main/java/lt/ausra/shapes/Circle.kt
+++ app/src/main/java/lt/ausra/shapes/Circle.kt
@@ -3,11 +3,10 @@ package lt.ausra.shapes
 import java.math.RoundingMode
 import kotlin.math.PI
 
-class Circle(var radius: Int) : Shape(0, 0) {
+class Circle(radius: Int) : Shape(radius, radius) {
 
     override fun calculateArea() {
-        area = (
-                radius * radius * PI
-                ).toBigDecimal().setScale(0, RoundingMode.HALF_UP).toInt()
+        super.calculateArea()
+        area *= PI.toBigDecimal().setScale(0, RoundingMode.HALF_UP).toInt()
     }
 }
